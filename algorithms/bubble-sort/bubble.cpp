6a9fd1999bcbbfc8b7f0994730cabdfff68de68c
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2026-04-29 16:47:00 +0600

    Create bubble.cpp

---
+++ algorithms/bubble-sort/bubble.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swapped = true;
+            }
+        }
+
+        // optimization: stop early if already sorted
+        if (!swapped)
+            break;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
