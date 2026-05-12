66a0e6f47ec027a22a2ad874066e55d395f34c21
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2026-05-12 21:35:33 +0600

    Implement quick sort algorithm in quick.py

---
+++ algorithms/quick-sort/quick.py
@@ -0,0 +1,32 @@
+def partition(arr, low, high):
+
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+
+    return i + 1
+
+
+def quick_sort(arr, low, high):
+
+    if low < high:
+
+        pivot_index = partition(arr, low, high)
+
+        quick_sort(arr, low, pivot_index - 1)
+        quick_sort(arr, pivot_index + 1, high)
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+quick_sort(arr, 0, len(arr) - 1)
+
+print("Sorted Array:")
+print(arr)
