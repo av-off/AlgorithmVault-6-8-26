5636405f4dbfa04b9dd665c565c1c6f7fb15e2c3
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2026-05-21 22:50:45 +0600

    Add SelectionSort class with selection sort method
    Implemented selection sort algorithm in Java.

---
+++ algorithms/selection-sort/selection.java
@@ -0,0 +1,28 @@
+public class SelectionSort {
+
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 6};
+
+        selectionSort(arr);
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
