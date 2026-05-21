4242d6bfb97b5ba5a63a657d2c9b76180fc46aed
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2026-05-21 22:46:25 +0600

    Create linear.java

---
+++ algorithms/linear-search/linear.java
@@ -0,0 +1,15 @@
+public class LinearSearch {
+
+    static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 6};
+
+        System.out.println("Index: " + linearSearch(arr, 9));
+    }
+}
